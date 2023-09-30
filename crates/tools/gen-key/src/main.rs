69b109c0af669af6072fb0d870d46b74d2301f06
Jeremy Chone <jeremy.chone@gmail.com>
2023-09-29 19:11:56 -0700

    . minor gen-key print change

--- crates/tools/gen-key/src/main.rs
+++ crates/tools/gen-key/src/main.rs
@@ -5,7 +5,7 @@ use rand::RngCore;
 fn main() -> Result<()> {
 	let mut key = [0u8; 64]; // 512 bits = 64 bytes
 	rand::thread_rng().fill_bytes(&mut key);
-	println!("\nGenerated key for HMAC:\n{key:?}");
+	println!("\nGenerated key from rand::thread_rng():\n{key:?}");
 
 	let b64u = b64u_encode(key);
 	println!("\nKey b64u encoded:\n{b64u}");
